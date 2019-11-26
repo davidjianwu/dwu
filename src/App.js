6834c6e869244823c94246a6b492dba06e38dd1c
David Wu <zjdavidwu@gmail.com>
2019-11-25 22:43:25 -0500

    cool stuff:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import pic from './good_img.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={"good_img.jpg"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Wussup<h1>
+        <img src={pic} className="App-logo" alt="logo" />
       </header>
     </div>
   );
