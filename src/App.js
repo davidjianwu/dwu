9abb88737ec118aa7d38e7b6559bcee6460d5f85
David Wu <zjdavidwu@gmail.com>
2019-12-03 00:18:39 -0500

    added backend fetch:

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import pic from './good_img.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Wussup</h1>
-      <img src={pic} className="pic" alt="pic" />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    let response = fetch('https://dwuserver.herokuapp.com/express_backend').then((data) => {
+      data.text().then((text) => this.setState({heading: text}))
+    })
+    this.state = {
+      heading: ''
+    }
+  }
+
+
+  render() {
+    console.log();
+    return (
+      <div className="App">
+        <h1>{this.state.heading}</h1>
+        <img src={pic} className="pic" alt="pic" />
+      </div>
+    );
+  }
 }
 
 export default App;
